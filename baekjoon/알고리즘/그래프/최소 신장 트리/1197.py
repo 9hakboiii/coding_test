d9fd02ccbdb2266a4a15e4ee6c1a619855fe6f99
9hakboiii <gyuhakarmy@gmail.com>
2025-09-01 12:57:46 +0900

    25.09.01 플로이드-위셜, 최소 신장 트리 문제풀이

---
+++ baekjoon/알고리즘/그래프/최소 신장 트리/1197.py
@@ -0,0 +1,39 @@
+from queue import PriorityQueue
+import sys
+input = sys.stdin.readline
+
+v, e = map(int, input().split())
+pq = PriorityQueue()
+parent = [0] * (v+1)
+
+for i in range(v+1):
+    parent[i] = i
+
+for i in range(e):
+    s, e, w = map(int, input().split())
+    pq.put((w, s, e))
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+    
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
+useEdge = 0
+result = 0
+
+while useEdge < v - 1:
+    w, s, e = pq.get()
+    if find(s) != find(e):
+        union(s, e)
+        result += w
+        useEdge += 1
+
+print(result)

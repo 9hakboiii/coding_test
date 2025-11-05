7936aff5e92038f846d8ff8f4c0cd973fb75ad06
9hakboiii <gyuhakarmy@gmail.com>
2025-11-05 10:35:08 +0900

    refact: 변수명 변경, 리스트 컨프리헨션으로 변경

--- solve/문제 풀이/집합과 맵 with 해시/10815.py
+++ solve/문제 풀이/집합과 맵 with 해시/10815.py
@@ -4,14 +4,8 @@ input = sys.stdin.readline
 n = int(input())
 my_card = set(map(int, input().split()))
 
-deck = int(input())
+m = int(input())
 target_card = list(map(int, input().split()))
 
-ans = []
-for card in target_card:
-    if card in my_card:
-        ans.append('1')
-    else:
-        ans.append('0')
-
+ans = ['1' if card in my_card else '0' for card in target_card]
 sys.stdout.write(' '.join(ans))
